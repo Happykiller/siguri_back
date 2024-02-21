abd4ca775a8d47c3247baf5e9514a92d0405b7e6
f.rosito@thomyris.com <f.rosito@thomyris.com>
2024-02-21 14:27:25 +0100

    fix: js ?

--- src/config/configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Configuration = void 0;
-var Configuration = /** @class */ (function () {
-    function Configuration() {
-    }
-    return Configuration;
-}());
-exports.Configuration = Configuration;
--- src/config/mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.conf = void 0;
-var conf = {
-    env: {
-        mode: 'mock'
-    }
-};
-exports.conf = conf;
